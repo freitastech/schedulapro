5ddbec95efddb920120b4b67ad204abb8849b23f
Jéssica Freitas <contato@freitastech.net>
2026-01-23 20:30:48 -0300

    feat(services): add validation requests for store and update

---
+++ app/Http/Requests/StoreServiceRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreServiceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:3', 'max:120'],
+            'duration_minutes' => ['required', 'integer', 'min:1', 'max:1440'],
+            'price_cents' => ['required', 'integer', 'min:0'],
+            'is_active' => ['sometimes', 'boolean'],
+        ];
+    }
+}
